cdd7a6d0f8aaf9e42a4f3f5223d9645a5d311217
wd <wdowny@mail.ru>
2020-06-07 21:34:58 +1000

    configuration

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=pdbase',
+    'username' => 'pdbase_sa',
+    'password' => 'Slava3_4',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
